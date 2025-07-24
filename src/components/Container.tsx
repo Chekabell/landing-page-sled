2e52cf4fe3d6edc9ba71f18b677109d6d963f343
Chekabell <nikita.toporischev@mail.ru>
2025-07-24 19:54:34 +0500

    Configure utility container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,8 +2,6 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   setup(props, { slots }) {
-    return () => (
-      <div class="container  mx-auto px-[0.667rem]">{slots.default ? slots.default() : null}</div>
-    )
+    return () => <div class="container">{slots.default ? slots.default() : null}</div>
   },
 })
