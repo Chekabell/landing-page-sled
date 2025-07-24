f148162ef9c42c604739408ed3892824dbcc6db0
Chekabell <nikita.toporischev@mail.ru>
2025-07-24 18:11:56 +0500

    Add base carousel functional

--- src/App.tsx
+++ src/App.tsx
@@ -3,105 +3,105 @@ import Container from './components/Container'
 import Sled from '@/assets/logo.svg'
 import Sled2 from '@/assets/logo2.png'
 import MockUp from '@/assets/MockUp.png'
+import CarouselAboutApplication from './components/CarouselAboutApplication'
 
 export default defineComponent({
   setup() {
-    return () => {
-      return (
-        <div class={'flex flex-col min-h-dvh overflow-hidden'}>
-          <main>
-            <header
-              class={
-                'fixed top-0 w-full py-[1.667rem] z-10 bg-blur backdrop-blur-[10px]   overflow-hidden '
-              }
-            >
-              <Container>
-                <div class="flex justify-between">
-                  <img src={Sled} alt="След" />
-                  <nav class={'flex items-center'}>
-                    <ul class={'flex gap-[3.5rem]'}>
-                      <li>
-                        <a href="#goal">Цель</a>
-                      </li>
-                      <li>
-                        <a href="#problems">Проблемы</a>
-                      </li>
-                      <li>
-                        <a href="#audiens">Аудитория</a>
-                      </li>
-                      <li>
-                        <a href="#about">О приложении</a>
-                      </li>
-                      <li>
-                        <a href="#advantages">Достижения</a>
-                      </li>
-                      <li>
-                        <a href="#contacts">Контакты</a>
-                      </li>
-                    </ul>
-                  </nav>
-                </div>
-              </Container>
-            </header>
-            <section
-              class={'flex flex-col h-[900px] relative py-[106px] bg-gradient'}
-              style={{
-                background:
-                  'radial-gradient(circle at right, white 0%, #C8D3FF 60%, rgba(124, 131, 159, 0.25) 100%)',
-              }}
-            >
-              <div class="flex relative">
-                <Container>
-                  <div class="flex items-center justify-center">
-                    {' '}
-                    <div class="flex flex-col">
-                      <img class={'max-w-[400px] h-[101px]'} src={Sled2} alt="След" />
-                      <h1 class={' font-extralight text-[2.7rem]'}>
-                        Приложение по созданию <br /> 3D-модели следа обуви
-                      </h1>
-                    </div>
-                    <div class="flex z-20">
-                      <img src={MockUp} alt="Телефон" />
-                    </div>
-                  </div>
-                </Container>
+    return () => (
+      <div class={'flex flex-col min-h-dvh overflow-hidden'}>
+        <main>
+          <header
+            class={
+              'fixed top-0 w-full py-[1.667rem] z-10 bg-blur backdrop-blur-[10px]   overflow-hidden '
+            }
+          >
+            <Container>
+              <div class="flex justify-between">
+                <img src={Sled} alt="След" />
+                <nav class={'flex items-center'}>
+                  <ul class={'flex gap-[3.5rem]'}>
+                    <li>
+                      <a href="#goal">Цель</a>
+                    </li>
+                    <li>
+                      <a href="#problems">Проблемы</a>
+                    </li>
+                    <li>
+                      <a href="#audiens">Аудитория</a>
+                    </li>
+                    <li>
+                      <a href="#about">О приложении</a>
+                    </li>
+                    <li>
+                      <a href="#advantages">Достижения</a>
+                    </li>
+                    <li>
+                      <a href="#contacts">Контакты</a>
+                    </li>
+                  </ul>
+                </nav>
               </div>
-              <div
-                class="absolute right-0 bg-gradient-hero w-[476px] top-0 h-full rounded-[30px]"
-                style={{
-                  background: 'linear-gradient(270deg, rgba(255, 255, 255, 0) 0%, #C8D3FF 100%)',
-                }}
-              />
-            </section>
-            <section>
+            </Container>
+          </header>
+          <section
+            class={'flex flex-col h-[900px] relative py-[106px] bg-gradient'}
+            style={{
+              background:
+                'radial-gradient(circle at right, white 0%, #C8D3FF 60%, rgba(124, 131, 159, 0.25) 100%)',
+            }}
+          >
+            <div class="flex relative">
               <Container>
-                <div class="flex flex-col">
-                  <h2
-                    class={'text-[2.7rem]  relative leading-[140%] text-blue font-semibold'}
-                    id="goal"
-                  >
-                    <span class="highlighted relative ">ЦЕЛЬ</span> ПРОЕКТА:
-                  </h2>
-
-                  <p class={'max-w-[734px] w-full font-light py-[1.4rem] leading-[100%]'}>
-                    Разработка MVP приложения, которое из 2D изображений следа обуви с разных
-                    ракурсов позволит создать 3D-модель следа обуви и определить с помощью
-                    ИИ его базовые параметры, такие как группа обуви, длина следа обуви, длина
-                    и ширина его составных частей и др. Так же приложение после анализа параметров
-                    следа обуви ИИ сможет их сопоставить с базой данных обуви и указать ссылку
-                    на определённую модель обуви, с которой будет выявлено совпадение.
-                  </p>
+                <div class="flex items-center justify-center">
+                  {' '}
+                  <div class="flex flex-col">
+                    <img class={'max-w-[400px] h-[101px]'} src={Sled2} alt="След" />
+                    <h1 class={' font-extralight text-[2.7rem]'}>
+                      Приложение по созданию <br /> 3D-модели следа обуви
+                    </h1>
+                  </div>
+                  <div class="flex z-20">
+                    <img src={MockUp} alt="Телефон" />
+                  </div>
                 </div>
               </Container>
-            </section>
-            <section>
-              <Container>
-                <div class="flex justify-between"></div>
-              </Container>
-            </section>
-          </main>
-        </div>
-      )
-    }
+            </div>
+            <div
+              class="absolute right-0 bg-gradient-hero w-[476px] top-0 h-full rounded-[30px]"
+              style={{
+                background: 'linear-gradient(270deg, rgba(255, 255, 255, 0) 0%, #C8D3FF 100%)',
+              }}
+            />
+          </section>
+          <section>
+            <Container>
+              <div class="flex flex-col">
+                <h2
+                  class={'text-[2.7rem]  relative leading-[140%] text-blue font-semibold'}
+                  id="goal"
+                >
+                  <span class="highlighted relative ">ЦЕЛЬ</span> ПРОЕКТА:
+                </h2>
+
+                <p class={'max-w-[734px] w-full font-light py-[1.4rem] leading-[100%]'}>
+                  Разработка MVP приложения, которое из 2D изображений следа обуви с разных ракурсов
+                  позволит создать 3D-модель следа обуви и определить с помощью ИИ его базовые
+                  параметры, такие как группа обуви, длина следа обуви, длина и ширина его составных
+                  частей и др. Так же приложение после анализа параметров следа обуви ИИ сможет
+                  их сопоставить с базой данных обуви и указать ссылку на определённую модель обуви,
+                  с которой будет выявлено совпадение.
+                </p>
+              </div>
+            </Container>
+          </section>
+          <section>
+            <Container>
+              <div class="flex justify-between"></div>
+            </Container>
+          </section>
+          <CarouselAboutApplication />
+        </main>
+      </div>
+    )
   },
 })
--- src/components/CarouselAboutApplication.tsx
+++ src/components/CarouselAboutApplication.tsx
@@ -1,29 +1,131 @@
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Navigation, Pagination, Autoplay } from 'swiper/modules'
 import { defineComponent } from 'vue'
 
-export default defineComponent({
-  setup() {
-    const isAndroid = ref<boolean>(true)
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 
-    const ToggleOS = (): void => {
-      isAndroid.value = !isAndroid.value
-    }
+export default defineComponent({
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    const slides = [
+      {
+        id: 1,
+        content: (
+          <Slide
+            images={['/src/assets/image/carousel/Android/1.png']}
+            text="Ключевыми функциями приложения SLED являются создание 3D-модели следа обуви и
+            анализ параметров следа обуви c помощью использования Искусственного интеллекта."
+          />
+        ),
+      },
+      {
+        id: 2,
+        content: (
+          <Slide
+            images={[
+              '/src/assets/image/carousel/Android/2-1.png',
+              '/src/assets/image/carousel/Android/2-2.png',
+              '/src/assets/image/carousel/Android/2-3.png',
+            ]}
+            text="При использовании приложения, сотруднику правоохранительных органов необходимо произвести
+            фотосъемку следа обуви c 5 разных ракурсов в количестве 5 изображений."
+          />
+        ),
+      },
+      {
+        id: 3,
+        content: (
+          <Slide
+            images={['/src/assets/image/carousel/Android/3.png']}
+            text="Далее после загрузки 5 изображений выпадет окно на выбора одной
+            из двух предложенных функций  приложения."
+          />
+        ),
+      },
+      {
+        id: 4,
+        content: (
+          <Slide
+            images={[
+              '/src/assets/image/carousel/Android/4-1.png',
+              '/src/assets/image/carousel/Android/4-2.png',
+              '/src/assets/image/carousel/Android/4-3.png',
+            ]}
+            text='В случае выбора функции "3D-модель" на основании выбранных изображений будет создана 3D-модель,
+            файл которой можно будет скачать на устройство для дальнейшего её использования
+            (формат файла  OBJ, STL и PLY).'
+          />
+        ),
+      },
+      {
+        id: 5,
+        content: (
+          <Slide
+            images={[
+              '/src/assets/image/carousel/Android/5-1.png',
+              '/src/assets/image/carousel/Android/5-2.png',
+              '/src/assets/image/carousel/Android/5-3.png',
+            ]}
+            text='В случае выбора функции "Искусственный интеллект" на основании выбранных изображений после их
+            анализа будут выведены данные параметров следа обуви, которые можно сканировать или скачать файлом
+            (формат файла TXT, DOC и PDF), и  ссылка обуви, с которой было обнаружено сходство с следом.'
+          />
+        ),
+      },
+      {
+        id: 6,
+        content: (
+          <Slide
+            images={['/src/assets/image/carousel/Android/6.png']}
+            text="Все созданные  3D-модели и данные ИИ будут сохранятся в истории приложения для
+            использования их по необходимости."
+          />
+        ),
+      },
+    ]
 
     return () => (
-      <div>
-        <h2 class="uppercase text-blue font-roboto-semi-bold font-semibold text-[2.7rem]">
-          Основные функции приложения
-        </h2>
+      <div class="h-screen flex flex-row">
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          slidesPerView={1}
+          spaceBetween={30}
+          navigation
+          pagination={{ clickable: true }}
+        >
+          {slides.map((slide) => (
+            <SwiperSlide key={slide.id}>{slide.content}</SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     )
   },
 })
 
-export const ButtonLogo = defineComponent({
+const Slide = defineComponent({
   props: {
-    isActive: { type: Boolean },
-    isAndroid: { type: Boolean },
+    text: String,
+    images: Array<string>,
   },
   setup(props) {
-    return () => <button></button>
+    return () => (
+      <div class="h-screen">
+        <div class="relative justify-center flex flex-row">
+          <div
+            class="absolute rounded-full h-85 w-85 top-20 z-[1]"
+            style="background: linear-gradient(0deg,rgba(255, 255, 255, 1) 0%, rgba(200, 211, 255, 1) 59%);"
+          />
+          {props.images?.map((image) => (
+            <img class="h-100 object-cover z-[2]" src={image}></img>
+          ))}
+        </div>
+        <div class="relative z-[2]">{props.text}</div>
+      </div>
+    )
   },
 })
