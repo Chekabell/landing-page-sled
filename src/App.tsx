223ea4b56fb2bdcbe61471fe8d4c406cff2746c1
Chekabell <nikita.toporischev@mail.ru>
2025-08-02 20:24:11 +0500

    Added adaptivity for Achievements

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import CarouselAboutApplication from './components/CarouselAboutApplication'
 import Header from './components/Header'
 import ProblemSolution from './components/ProblemSolution'
 import AchievementsSectionBegin from './components/AchievementsSectionBegin'
+import AchievementsSectionContinue from './components/AchievementsSectionContinue'
 
 export default defineComponent({
   setup() {
@@ -102,8 +103,15 @@ export default defineComponent({
           </section>
 
           <ProblemSolution />
-          <CarouselAboutApplication />
-          <AchievementsSectionBegin />
+          <section class="sm:h-[80vh] lg:h-screen">
+            <CarouselAboutApplication />
+          </section>
+          <section class="h-screen my-2">
+            <AchievementsSectionBegin />
+          </section>
+          <section class="my-10">
+            <AchievementsSectionContinue />
+          </section>
         </main>
       </div>
     )
